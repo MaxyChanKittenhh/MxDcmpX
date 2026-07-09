202ff2098cc7c658a37153d5a8422a6f38d2069c
MaxyChanKittenhh <babitadubey483@gmail.com>
2026-07-09 18:57:48 +0530

    Update engine.lua

--- engine.lua
+++ engine.lua
@@ -1,1 +1,23 @@
+-- engine.lua
+local Engine = {}
 
+function Engine.Save(Config)
+    local Params = { 
+        RepoURL = "https://raw.githubusercontent.com/luau/UniversalSynSaveInstance/main/", 
+        SSI = "saveinstance", 
+    } 
+    
+    -- Safely attempt to load the USSI script
+    local success, synsaveinstance = pcall(function()
+        return loadstring(game:HttpGet(Params.RepoURL .. Params.SSI .. ".luau", true), Params.SSI)()
+    end)
+
+    if success then
+        print("[MXDCMPX] Engine loaded successfully. Starting extraction...")
+        synsaveinstance(Config)
+    else
+        warn("[MXDCMPX] Engine Error: Failed to load UniversalSynSaveInstance.")
+    end
+end
+
+return Engine
